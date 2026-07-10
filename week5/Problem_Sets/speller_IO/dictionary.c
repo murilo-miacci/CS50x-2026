6391bbaaad5ecd76c9add6f9f23fbaf5f2a25d9c
Murilo Pereira Miacci <murilo.miacci@gmail.com>
2026-07-10 16:40:04 -0300

    Add solutions for Week 5 problem sets

---
+++ week5/Problem_Sets/speller_IO/dictionary.c
@@ -0,0 +1,128 @@
+// Implements a dictionary's functionality
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+#define N 729
+node *table[N];
+unsigned int word_count = 0;
+
+unsigned int hash(const char *word);
+
+
+bool check(const char *word)
+{
+    unsigned int index = hash(word);
+    node *cursor = table[index];
+
+    while (cursor != NULL)
+    {
+        if (strcasecmp(word, cursor->word) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            cursor = cursor->next;
+        }
+    }
+
+    return false;
+}
+
+
+unsigned int hash(const char *word)
+{
+    unsigned int first = 0;
+    unsigned int second = 0;
+
+    if (word[0] == '\'')
+    {
+        first = 26;
+    }
+    else if (isalpha((unsigned char) word[0]))
+    {
+        first = toupper((unsigned char) word[0]) - 'A';
+    }
+    
+    if (word[1] == '\'')
+    {
+        second = 26;
+    }
+    else if (word[1] != '\0' && isalpha((unsigned char) word[1]))
+    {
+        second = toupper((unsigned char) word[1]) - 'A';
+    }
+
+    return first * 27 + second;
+}
+
+
+bool load(const char *dictionary)
+{
+    FILE *source = fopen(dictionary, "r");
+    if (source == NULL) return false;
+
+    char word[LENGTH + 1];
+
+    while (fscanf(source, "%45s", word) == 1)
+    {
+        node *new_node = malloc(sizeof(node));
+        if (new_node == NULL)
+        {
+            fclose(source);
+            unload();
+            return false;
+        }
+
+        strcpy(new_node->word, word);
+
+        unsigned int index = hash(word);
+        new_node->next = table[index];
+        table[index] = new_node;
+
+        word_count++;
+    }
+
+    fclose(source);
+    return true;
+}
+
+
+unsigned int size(void)
+{
+    return word_count;
+}
+
+
+bool unload(void)
+{
+    for (unsigned int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+
+        while (cursor != NULL)
+        {
+            node *next = cursor->next;
+            free(cursor);
+            cursor = next;
+        }
+
+        table[i] = NULL;
+    }
+    
+    word_count = 0;
+    return true;
+}
